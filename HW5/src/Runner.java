b70299b6b3eec9edebea31fc89aa3a98d32c1006
Aleksandr Aksionenko <gingo0207@gmail.com>
2020-07-23 14:36:27 +0300

    Added HW5

--- HW5/src/Runner.java
+++ HW5/src/Runner.java
@@ -1,10 +1,10 @@
 public class Runner {
-    
+
     public static void main(String[] args) {
-        Car[] cars = {new Car(101, "BMV", "116d", 2010, "Red", 300000, "ax123ac"),
+        Car[] cars = {new Car(101, "BMW", "116d", 2010, "Red", 300000, "ax123ac"),
                 new Car(302, "Mercedes-Benz", "w140", 2019, "Blue", 205000, "ax1833bv"),
                 new Car(1203, "AUDI", "1162d", 2019, "Yellow", 600000, "ax9323ou"),
-                new Car(4204, "BMV", "114X", 2018, "Red", 300000, "ax1202v"),
+                new Car(4204, "BMW", "114X", 2018, "Red", 300000, "ax1202v"),
                 new Car(305, "Lexus", "1685U", 2011, "Black", 400000, "ax1203kv")};
 
         CarProcessor proc = new CarProcessor();
